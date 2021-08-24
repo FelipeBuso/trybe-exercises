1d5a88b65870ddebc3acbdda19b2f8ee99d58231
Felipe Buso <felipe_buso@hotmail.com>
2021-08-24 16:25:42 -0300

    exercicios de fixação bloco 21 dia 1

---
+++ Back-end/Bloco_21 - Funções_SQL_ Joins_Subqueries/21.1 - Funções_mais_usadas_no_SQL/21.1-Exercícios_de_fixação/manipulacao_strings.sql
@@ -0,0 +1,5 @@
+SELECT UCASE('trybe');
+SELECT replace('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
+SELECT length('Uma frase qualquer');
+select substring('A linguagem JavaScript está entre as mais usadas', 13, 10);
+SELECT lcase('RUA NORTE 1500, SÃO PAULO, BRASIL');
