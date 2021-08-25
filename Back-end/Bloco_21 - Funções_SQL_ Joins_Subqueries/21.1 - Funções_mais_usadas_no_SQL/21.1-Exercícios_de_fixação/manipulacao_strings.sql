28ceaef2d4f41003ccfc5557c64c594d75a8fba3
Felipe Buso <felipe_buso@hotmail.com>
2021-08-25 17:16:11 -0300

    Delete Back-end directory

--- Back-end/Bloco_21 - Funções_SQL_ Joins_Subqueries/21.1 - Funções_mais_usadas_no_SQL/21.1-Exercícios_de_fixação/manipulacao_strings.sql
+++
@@ -1,5 +0,0 @@
-SELECT UCASE('trybe');
-SELECT replace('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
-SELECT length('Uma frase qualquer');
-select substring('A linguagem JavaScript está entre as mais usadas', 13, 10);
-SELECT lcase('RUA NORTE 1500, SÃO PAULO, BRASIL');
