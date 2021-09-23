cff240985a32ed7f03c4f533ccbef45b86bf4e1e
Felipe Buso <felipe_buso@hotmail.com>
2021-09-23 18:49:45 -0300

    exercicio 5

--- Back-end/Bloco_26-bloco-26-Introducao_ao_Desenvolvimento_Web_com_NodeJS/26.1-NodeJS_Introdução/26.1-Exercicios/imc.js
+++ Back-end/Bloco_26-bloco-26-Introducao_ao_Desenvolvimento_Web_com_NodeJS/26.1-NodeJS_Introdução/26.1-Exercicios/imc.js
@@ -1,5 +1,23 @@
 const { questionFloat } = require('readline-sync');
-const calcIMC = (peso, altura) => peso / altura ^ 2;
+
+function calcIMC (peso, altura) {
+  let situação = '';
+  const imc = peso / (altura * altura)
+  if (imc < 18.5 ) {
+    situação = 'Abaixo do peso (magreza)';
+  } else if ( imc >= 18.5 && imc < 25 ) {
+    situação = 'Peso normal';
+  } else if ( imc >= 25 && imc < 30 ) {
+    situação = 'Acima do peso (sobrepeso)';
+  } else if ( imc >= 30 && imc < 35 ) {
+    situação = 'Obesidade grau I ';
+  } else if ( imc >= 35 && imc < 40 ) {
+    situação = 'Obesidade grau II ';
+  } else {
+    situação = 'Obesidade grau III ';
+  }
+  return ` Seu IMC é ${imc} - ${situação}`
+};
 
 const userPeso = questionFloat("Qual seu peso?");
 const userAltura = questionFloat("Qual sua altura?");
