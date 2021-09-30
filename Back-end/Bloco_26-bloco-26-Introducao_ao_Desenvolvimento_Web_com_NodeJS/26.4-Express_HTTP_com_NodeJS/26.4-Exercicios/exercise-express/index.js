be04e0d15e329a9bd07af4268a00218d0a7009a9
Felipe Buso <felipe_buso@hotmail.com>
2021-09-30 14:58:08 -0300

    exercicio 3

--- Back-end/Bloco_26-bloco-26-Introducao_ao_Desenvolvimento_Web_com_NodeJS/26.4-Express_HTTP_com_NodeJS/26.4-Exercicios/exercise-express/index.js
+++ Back-end/Bloco_26-bloco-26-Introducao_ao_Desenvolvimento_Web_com_NodeJS/26.4-Express_HTTP_com_NodeJS/26.4-Exercicios/exercise-express/index.js
@@ -15,4 +15,12 @@ app.post('/hello', (req, res) => {
   res.status(200).json({ message: `Hello, ${name}`});
 });
 
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (age < 18 ) return res.status(401).json({ message: 'Unauthorized' });
+
+  res.status(200).json({ message: `Hello, ${name}`});
+})
+
 app.listen(3000, () => console.log('Aplicação ouvindo porta 3000'));
