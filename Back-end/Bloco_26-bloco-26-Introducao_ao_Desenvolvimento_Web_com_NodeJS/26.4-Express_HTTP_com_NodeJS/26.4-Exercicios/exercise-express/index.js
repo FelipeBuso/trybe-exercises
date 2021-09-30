0bdf1a9b88cbf15f2a62e9ff1f8eed81312e4b05
Felipe Buso <felipe_buso@hotmail.com>
2021-09-30 16:22:02 -0300

    exercicios 7 e 8

--- Back-end/Bloco_26-bloco-26-Introducao_ao_Desenvolvimento_Web_com_NodeJS/26.4-Express_HTTP_com_NodeJS/26.4-Exercicios/exercise-express/index.js
+++ Back-end/Bloco_26-bloco-26-Introducao_ao_Desenvolvimento_Web_com_NodeJS/26.4-Express_HTTP_com_NodeJS/26.4-Exercicios/exercise-express/index.js
@@ -13,6 +13,11 @@ async function getSimpsons() {
   return readSimpsons;
 }
 
+async function updateSimpsons(newSimpsons) {
+  await fs.writeFile('./simpsons.json', JSON.stringify(newSimpsons));
+};
+
+
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
 });
@@ -39,11 +44,40 @@ app.put('/users/:name/:age', (req, res) => {
 });
 
 app.get('/simpsons', async (req, res) => {
-  const simpsons = await getSimpsons()
+  const simpsons = await getSimpsons();
 
-  if (!simpsons) return res.status(500).json('Internal Server Error')
+  if (!simpsons) return res.status(500).json('Internal Server Error');
   
   res.status(200).json(simpsons);
+})
+
+app.get('/simpsons/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const simpsons = await getSimpsons();
+  if (!simpsons) return res.status(500).json('Internal Server Error');
+
+  const findSimpson = simpsons.find((s) => s.id === id );
+  if(!findSimpson) return res.status(404).json({ message: 'simpson not found'});
+
+  res.status(200).json(findSimpson);
+
+})
+
+app.post('/simpsons', async (req, res) => {
+  const { id, name } = req.body;
+
+  const simpsons = await getSimpsons();
+  if (!simpsons) return res.status(500).send('Internal Server Error')
+
+  const checkSimpson = simpsons.find((s) => s.id.includes(id));
+  if(checkSimpson) return res.status(409).json({ message: "id already exist"});
+
+  const newSimpson = { 'id': id, 'name': name };
+  simpsons.push(newSimpson);
+  updateSimpsons(simpsons);
+
+  res.status(204).end();
 })
 
 app.listen(3000, () => console.log('Aplicação ouvindo porta 3000'));
