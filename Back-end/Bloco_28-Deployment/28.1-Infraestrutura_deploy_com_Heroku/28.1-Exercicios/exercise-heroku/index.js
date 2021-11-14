90654e29de48ff1a6685e185068299716d1818fa
Felipe Buso <felipe_buso@hotmail.com>
2021-11-14 12:43:29 -0300

    correção git

--- Back-end/Bloco_28-Deployment/28.1-Infraestrutura_deploy_com_Heroku/28.1-Exercicios/exercise-heroku/index.js
+++ Back-end/Bloco_28-Deployment/28.1-Infraestrutura_deploy_com_Heroku/28.1-Exercicios/exercise-heroku/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Está vivo!!!');
+  res.send(`Está vivo na ${PORT} ${process.env.SERVER_ENV || 'não achou'}!!!`);
 });
 
 app.listen(PORT, console.log(`rodando na porta: ${PORT}`));
