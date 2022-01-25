972b5b7df5822dbce05b217993e8838f2689c36e
felipebuso <felipe_buso@hotmail.com>
2022-01-25 08:37:03 -0300

    exercicio4

--- Ciencia_da_Computacao/Bloco_32-Introducao_Python/32.2-Entrada_Saida_de_Dados/32.2-Exercicios/exercicio4.py
+++ Ciencia_da_Computacao/Bloco_32-Introducao_Python/32.2-Entrada_Saida_de_Dados/32.2-Exercicios/exercicio4.py
@@ -1,14 +1,8 @@
 import json
 
+# books = []
 categories = set()
 with open("file.json", "r") as file:
-    books = json.load(file)
-    
-    for line in books:
-        print(line["categories"].split(","))
-        # for book in line:
-        #     category = book["categories"].split(",")
-        #     # categories.add(book["categories"])
-        #     print(categories)
-
-# print(categories)
+    content = file.read()
+    books = json.loads(content)
+    print(books[0])
